caa2f63bc12472c8655dc9903f00c4e81266dc4f
Joschua Fink <joschua.fink@gmx.de>
2018-01-31 15:01:49 +0100

    format source code

--- keylogger.c
+++ keylogger.c
@@ -5,39 +5,42 @@
 #include <errno.h>
 #include <linux/input.h>
 
-int main(int argc, char** argv){
+int main(int argc, char** argv) {
 
-    int fd, bytes;
-    struct input_event data;
+	int fd, bytes;
+	struct input_event data;
 
-    const char *pDevice = "/dev/input/event1";
+	const char *pDevice = "/dev/input/event1";
 
-    // Open Keyboard
-    fd = open(pDevice, O_RDONLY | O_NONBLOCK);
-    if(fd == -1){
+	// Open Keyboard
+	fd = open(pDevice, O_RDONLY | O_NONBLOCK);
 
-        printf("ERROR Opening %s\n", pDevice);
-        return -1;
-    }
+	if(fd == -1) {
 
-    while(1)
-    {
-        // Read Keyboard Data
-        bytes = read(fd, &data, sizeof(data));
-        if(bytes > 0)
-        {
-	    if(data.value == KEY_W){
-	    	printf(" DER W-KEY WURDE GEDRUECKT!!!\n ");
-	    }
-            printf("Keypress value=%x, type=%x, code=%x\n", data.value, data.type, data.code);
-        }
-        else
-        {
-            // Nothing read
-	    printf("Nothing to read\n");
-            sleep(1);
-        }
-    }
+	printf("ERROR Opening %s\n", pDevice);
+	return -1;
+	}
 
-    return 0;
- }
+	while(1) {
+		// Read Keyboard Data
+		bytes = read(fd, &data, sizeof(data));
+		if(bytes > 0) {
+
+			if(data.value == KEY_W) {
+				printf(" DER W-KEY WURDE GEDRUECKT!!!\n ");
+	
+			}
+	
+		printf("Keypress value=%x, type=%x, code=%x\n", data.value, data.type, data.code);
+
+		}
+
+		else {
+			// Nothing read
+			printf("Nothing to read\n");
+			sleep(1);
+		}
+	}
+
+	return 0;
+}
